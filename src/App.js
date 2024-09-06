400a8468afce966ffaf6ed00eeaea156fe7fc055
서지호 <jihoseo2006@gmail.com>
2024-09-06 10:31:45 +0900

    feat: 컴포넌트 구성 세팅

--- src/App.js
+++ src/App.js
@@ -1,13 +1,25 @@
-import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import Home from './components/Home';
+import About from './components/About';
+import Support from './components/Support';
+import Feature from './components/Feature';
+import Feedback from './components/Feedback';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        hello tailwind!
-      </h1>
-    </div>
+    <>
+      <Navbar />
+      <Home />
+      <About />
+      <Support />
+      <Feature />
+      <Feedback />
+      <Contact />
+      <Footer />
+    </>
   );
 }
 
