1fe0fe38b7b9d1b2f2a580c845fb405d0fed43ec
서지호 <jihoseo2006@gmail.com>
2024-09-09 18:22:54 +0900

    feat: feature 컴포넌트 퍼블리싱

--- src/components/Feedback.js
+++ src/components/Feedback.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Feedback = () => {
   return (
-    <div>Feedback</div>
+    <section name="testimonials" className='w-full my-24 text-white bg-slate-700'>
+      <div className='max-w-6xl px-5 mx-auto text-center py-12'>
+        <h2 className='text-4xl font-bold'>
+          Testimonials
+        </h2>
+      </div>
+    </section>
   )
 }
 
